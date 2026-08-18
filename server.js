5951ab14a004746388d3e0d5068891d079e37f4c
LockVision <hassanelmaayati@gmail.com>
2026-08-18 09:42:31 +0300

    connected the DB, express, set a port, ready to go

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-require
+require('dotenv').config()
+
+const mongoose=require('mongoose')
+const express=require('express')
+const app = express()
+
+mongoose.connect(process.env.MONGODB_URI)
+mongoose.connection.on('connected',()=>{
+  console.log(`connected to MongoDB ${mongoose.connection.name}.`)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT,()=>{
+  console.log(`the express app is ready on port ${process.env.PORT} YAY!`)
+})
